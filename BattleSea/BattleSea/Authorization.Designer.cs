a095d71f8004df28f6855e36a59d34f31b0b1c6d
fedotovdanil570 <fedotovdanil570@gmail.com>
2016-11-23 23:07:29 +0400

    Кое-что подправил

--- BattleSea/BattleSea/Authorization.Designer.cs
+++ BattleSea/BattleSea/Authorization.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BattleSea
 {
-    partial class Form1
+    partial class Authorization
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -31,21 +31,20 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(87, 122);
+            this.button2.Location = new System.Drawing.Point(119, 90);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(219, 32);
+            this.button2.Size = new System.Drawing.Size(152, 26);
             this.button2.TabIndex = 1;
             this.button2.Text = "Вход";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 79);
+            this.textBox1.Location = new System.Drawing.Point(82, 52);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(219, 20);
             this.textBox1.TabIndex = 2;
@@ -53,35 +52,24 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(146, 63);
+            this.label1.Location = new System.Drawing.Point(141, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Имя пользователя";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(87, 160);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 32);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Выход";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
+            // Authorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 206);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(388, 149);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "Authorization";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Авторизация";
-            
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,7 +80,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
